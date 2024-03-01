8e710fd84e79cd400235d0ce06bb8755cf739908
Manish Kumar <manishkumaar69@gmail.com>
2024-03-01 22:13:43 +0100

    modify IAM

--- Kubernetes.tf
+++ Kubernetes.tf
@@ -3,3 +3,19 @@ provider "aws" {
   profile    = "thesis"
 }
 
+resource "aws_iam_role" "master" {
+  name = "eks-master"
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "eks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
