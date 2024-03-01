c1501ce89109e83d5bc0d47c0856578a1cf1612f
Manish Kumar <manishkumaar69@gmail.com>
2024-03-01 22:02:36 +0100

    modify variables

--- variables.tf
+++ variables.tf
@@ -4,3 +4,20 @@ variable "aws-region" {
   default     = "eu-west-1"
 }
 
+/*variable "aws-profile" {
+  description = "The name of the AWS shared credentials account."
+  type        = AWS-Test
+}*/
+
+variable "subnet_id1" {
+  description = "ID of the existing subnet"
+  type        = string
+  default     = "subnet-010cf1202660fcdb4"
+}
+
+variable "subnet_id2" {
+  description = "ID of the existing subnet"
+  type        = string
+  default     = "subnet-0d1104e1037af6b4f"
+}
+
